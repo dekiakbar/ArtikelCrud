714da96ca2483663a8dd8ee271348e76940ee30d
deki <dekiakbar@linuxmail.org>
2018-02-24 21:26:29 +0700

    model, kontroller,dan migrasi

---
+++ database/migrations/2018_02_24_054614_artikel_crud.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ArtikelCrud extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ArtikelCrud', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('judul');
+            $table->string('kutipan');
+            $table->string('slug');
+            $table->integer('kategori_id')->unique();
+            $table->string('isi');
+            $table->string('meta_keyword');
+            $table->string('meta_deskripsi');
+            $table->string('foto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ArtikelCrud');
+    }
+}
