59db6fec1ba3c1ce6b4243d81be7fa98274e5016
deki <dekiakbar@linuxmail.org>
2018-02-26 19:13:37 +0700

    UI landing done

--- resources/views/web/master.blade.php
+++ resources/views/web/master.blade.php
@@ -99,15 +99,122 @@
 					</div>
 				</div>
 			</div>
+
+			<div class="ui segment raised center aligned">
+				<h4 class="ui tiny horizontal divider header">
+					<i class="share icon"></i>
+					Share
+				</h4>
+
+				<div class="ui horizontal list">
+					<div class="item">
+						<a ><i class="big facebook icon"></i></a>
+						<a ><i class="big twitter square icon"></i></a>
+						<a ><i class="big linkedin square icon"></i></a>
+						<a ><i class="big telegram icon"></i></a>
+					</div>
+				</div>
+			</div>
 		</div>
 
 		<div class="twelve wide column">
-			<div class="ui segment"></div>
+			<div class="ui segment">
+				<h3 class="ui header">
+					<a>Judul JudulJudulJudulJudulJudulJudul</a>
+					<span class="sub header">Di post pada 12:12:12 21-12-2018</span>
+				</h3>
+
+				<div class="ui divider"></div>
+
+				<img class="ui medium left floated rounded image" src="https://dummyimage.com/1920x1080/000/fff">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse</p>
+
+				<div class="ui divider"></div>
+				<div class="netadata">
+					<a class="ui tiny teal tag label">Tag</a>
+					<a class="ui tiny teal tag label">Tag</a>
+					<a class="ui tiny teal tag label">Tag</a>
+					<div style="float: right;">
+						<a class="ui tiny label blue">Pemrograman</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="ui segment">
+				<h3 class="ui header">
+					<a>Judul JudulJudulJudulJudulJudulJudul</a>
+					<span class="sub header">Di post pada 12:12:12 21-12-2018</span>
+				</h3>
+
+				<div class="ui divider"></div>
+
+				<img class="ui medium left floated rounded image" src="https://dummyimage.com/1920x1080/000/fff">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse</p>
+
+				<div class="ui divider"></div>
+				<div class="netadata">
+					<a class="ui tiny teal tag label">Tag</a>
+					<a class="ui tiny teal tag label">Tag</a>
+					<a class="ui tiny teal tag label">Tag</a>
+					<div style="float: right;">
+						<a class="ui tiny label blue">Pemrograman</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="ui segment">
+				<h3 class="ui header">
+					<a>Judul JudulJudulJudulJudulJudulJudul</a>
+					<span class="sub header">Di post pada 12:12:12 21-12-2018</span>
+				</h3>
+
+				<div class="ui divider"></div>
+
+				<img class="ui medium left floated rounded image" src="https://dummyimage.com/1920x1080/000/fff">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse</p>
+
+				<div class="ui divider"></div>
+				<div class="netadata">
+					<a class="ui tiny teal tag label">Tag</a>
+					<a class="ui tiny teal tag label">Tag</a>
+					<a class="ui tiny teal tag label">Tag</a>
+					<div style="float: right;">
+						<a class="ui tiny label blue">Pemrograman</a>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 
 	<footer style="margin-top: 10px;">
-		<div class="ui segment center aligned">
+		<div class="ui segment center aligned blue">
 			<p>&copy;{!! date('Y') !!} 
 				<a href="https://dekiakbar.com">Deki Akbar</a> | Made With :
 				<a target="_blank" href="https://Laravel.com">Laravel</a>
