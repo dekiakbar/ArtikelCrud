c057fe6968f172fde8c5c3494bc78e9b09218160
deki <dekiakbar@linuxmail.org>
2018-03-04 13:35:09 +0700

    add page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>404 | Not Found</title>
+	<link href="https://afeld.github.io/emoji-css/emoji.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+	<div class="ui text container">
+		<div class="ui stackable centered grid">
+			<div class="ui segment teal raised">
+				<h1 style="margin-top: 10px;"><i class="em em-hushed"></i></h1>
+				<h1 class="header">404 | Not Found</h1>
+				<div class="ui divider"></div>
+				<div class="sub header">Maaf halaman yang anda minta tidak ditemukan</div>
+				<div class="sun header">Kami Yakin ini hanya sebuah kesalahan</div>
+				<div class="sun header">Klik tombol Home untuk kembali ke Home Page</div>
+				<div class="ui divider"></div>
+				<div class="center aligned">
+					<a href="{{ url('/') }}" class="ui button animated blue inverted">
+						<div class="hidden content">
+							<i class="home icon"></i>
+						</div>
+						
+						<div class="visible content">
+							Home    
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
