db0bbd472e395672f3177d4793054a74be0440f0
deki <dekiakbar@linuxmail.org>
2018-03-04 23:49:19 +0700

    + variable domain,quotes dan fix function search tag

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -13,6 +13,10 @@ use Chencha\Share\ShareFacade as Share;
 
 class WebController extends Controller
 {
+    //setting nama domain dan quote share
+    private $nama_domain= 'http://localhost:8000/';
+    private $quote_share= 'Artikel Laravel';
+
     public function tampil(Request $request)
     {
     	$tags = Tag::all();
@@ -20,7 +24,7 @@ class WebController extends Controller
     	$barus = Artikel::latest()->take(3)->get();
     	$artikels = Artikel::with('kategori')->paginate(5);
 
-        $bagikan  = Share::load('http://localhost:8000/', 'Artikel Laravel')
+        $bagikan  = Share::load($this->nama_domain, $this->quote_share)
                     ->services('facebook','twitter','linkedin','telegram');
         $bagikan  = (object) $bagikan;
 
@@ -37,7 +41,7 @@ class WebController extends Controller
     				->select('*','artikel.slug as slug','kategori.slug as kategori_slug')
     				->where('kategori.slug',$slug)->paginate(5);
 
-        $bagikan  = Share::load('http://localhost:8000/kategori/'.$slug, 'Artikel Laravel')
+        $bagikan  = Share::load($this->nama_domain.'kategori/'.$slug, $this->quote_share)
                     ->services('facebook','twitter','linkedin','telegram');
         $bagikan  = (object) $bagikan;
 
@@ -55,7 +59,7 @@ class WebController extends Controller
                     ->whereRaw('FIND_IN_SET('.$idTag->id.',tag_id)')
                     ->paginate(5);
 
-        $bagikan  = Share::load('http://localhost:8000/tag/'.$idTag->slug, 'Artikel Laravel')
+        $bagikan  = Share::load($this->nama_domain.'tag/'.$idTag->slug, $this->quote_share)
                     ->services('facebook','twitter','linkedin','telegram');
         $bagikan  = (object) $bagikan;
 
@@ -66,15 +70,23 @@ class WebController extends Controller
     public function cari(Request $request)
     {
         $cari = $request->input('cari');
+        $tgaId = Tag::where('slug','like', '%'.$cari.'%')->first();
+
         $tags = Tag::all();
         $kategoris = Kategori::all();
         $barus = Artikel::latest()->take(3)->get();
         $artikels = Artikel::select('kutipan','slug','judul','created_at','tag_id','kategori_id','foto')
-                          ->whereHas('kategori', function($query) use($cari) {$query->where('nama_kategori', 'like', '%'.$cari.'%');})
-                          ->orWhereHas('tag', function($query) use($cari) {$query->where('nama_tag', 'like', '%'.$cari.'%');})
-                          ->orWhere("judul", "LIKE","%$cari%")->orderBy('created_at', 'desc')->paginate(5);
+                            ->whereRaw('FIND_IN_SET('.$tgaId->id.', tag_id)')
+                            ->orwhereHas('kategori', function($query) use($cari) {
+                                $query->where('nama_kategori', 'like', '%'.$cari.'%');
+                            })
+                            ->orWhereHas('tag', function($query) use($cari) {
+                                $query->where('nama_tag', 'like', '%'.$cari.'%');
+                            })
+                            ->orWhere("judul", "LIKE","%$cari%")
+                            ->orderBy('created_at', 'desc')->paginate(5);
 
-        $bagikan  = Share::load('http://localhost:8000/', 'Artikel Laravel')
+        $bagikan  = Share::load($this->nama_domain, $this->quote_share)
                     ->services('facebook','twitter','linkedin','telegram');
         $bagikan  = (object) $bagikan;
 
@@ -89,7 +101,7 @@ class WebController extends Controller
         $barus = Artikel::latest()->take(3)->get();
         $artikel = Artikel::with('kategori')->where('artikel.slug',$slug)->firstOrFail();
 
-        $bagikan  = Share::load('http://localhost:8000/detail/'.$slug, 'Artikel Laravel')
+        $bagikan  = Share::load($this->nama_domain.'detail/'.$slug, $this->quote_share)
                     ->services('facebook','twitter','linkedin','telegram');
         $bagikan  = (object) $bagikan;
 
