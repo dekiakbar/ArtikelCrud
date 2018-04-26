5de58204bcbb0d0b19091d30c1886d80aed535e5
dekiakbar <dekiakbar@linuxmail.org>
2018-04-26 20:19:00 +0700

    puyeng

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://laravel-artikel.herokuapp.com'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
