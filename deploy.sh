e6d98565da6866e11a1f2dc570000bc8005a69a7
Deki Akbar <dekiakbar@linuxmail.org>
2021-03-28 16:04:22 +0700

    add deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,9 @@ php artisan optimize &&
 php artisan config:clear &&
 php artisan cache:clear &&
 php artisan route:clear &&
+
+# purge pagespeed cache
+sudo rm -rf /var/cache/ngx_pagespeed/*
+# restart nginx
+sudo service nginx restart &&
 date
